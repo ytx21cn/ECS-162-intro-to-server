eabbb54f3ca49bbcfce4383aac32f07e0202d628
Tianxing Yang <5900105+ytx21cn@users.noreply.github.com>
2019-04-29 21:30:20 -0700

    added given assignment files

---
+++ miniServer3.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const port = 30057
+
+function queryHandler(req, res, next) {
+    let url = req.url;
+    let qObj = req.query;
+    console.log(qObj);
+    if (qObj.animal != undefined) {
+	res.json( {"beast" : qObj.animal} );
+    }
+    else {
+	next();
+    }
+}
+
+function fileNotFound(req, res) {
+    let url = req.url;
+    res.type('text/plain');
+    res.status(404);
+    res.send('Cannot find '+url);
+    }
+
+// put together the server pipeline
+const app = express()
+app.use(express.static('public'));  // can I find a static file? 
+app.get('/query', queryHandler );   // if not, is it a valid query?
+app.use( fileNotFound );            // otherwise not found
+
+app.listen(port, function (){console.log('Listening...');} )
+ 
